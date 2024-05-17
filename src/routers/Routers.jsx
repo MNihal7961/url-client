f02315c9e9bff45e724dd6807123f290bfa8660f
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-18 01:21:26 +0530

    designed login page

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import MyUrls from "../pages/MyUrls";
 
 const Routers = () => {
   return (
-    <div>Routers</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/urls" element={<MyUrls />} />
+    </Routes>
+  );
+};
 
-export default Routers
+export default Routers;
