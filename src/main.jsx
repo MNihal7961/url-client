7a5fda52773cbb5e706153a7631fa94702b9acde
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-18 01:13:23 +0530

    setup the provider and toast container

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { presistor, store } from "./redux/store.js";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement)?.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate persistor={presistor} loading={null}>
+          <BrowserRouter>
+            <ToastContainer
+              theme="dark"
+              position="top-right"
+              autoClose={3000}
+              closeOnClick
+              pauseOnHover={false}
+            />
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+}
