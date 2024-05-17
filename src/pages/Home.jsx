f4f707a20d6cfb6bef20bb4a6130866db9b5f77e
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-18 01:32:51 +0530

    designed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import AddUrl from "../components/Addurl";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <AddUrl />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
